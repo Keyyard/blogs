bd14684bf45f2d5e8061def87030d3363af24b70
Keyyard <keyyard8888@gmail.com>
2024-10-18 19:08:13 +0700

    Google Analysis

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -24,6 +24,15 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+                <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z3K6D9SXF4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Z3K6D9SXF4');
+        </script>
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
