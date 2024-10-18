29a4c9794b725da11d0a8e95c30882f608b48920
Keyyard <keyyard8888@gmail.com>
2024-10-19 05:29:59 +0700

    Refactor Giscus comments component in footer: Update theme option to remove preferred color scheme

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -41,7 +41,7 @@ export default (() => {
         data-reactions-enabled="1"
         data-emit-metadata="0"
         data-input-position="bottom"
-        data-theme="preferred_color_scheme"
+        data-theme=""
         data-lang="en"
         crossorigin="anonymous"
         async>
