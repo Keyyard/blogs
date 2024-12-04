55bf052511148aef0a8af110b841f42210124f8f
Keyyard <keyyard8888@gmail.com>
2024-12-04 13:50:14 +0700

    Refactor Comments component: streamline implementation and improve script handling

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -1,25 +1,23 @@
 import React, { useEffect } from 'react';
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types";
 
-export default (() => {
-  const Comments: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
-    useEffect(() => {
-      const script = document.createElement('script');
-      script.src = "https://cdn.commoninja.com/sdk/latest/commonninja.js";
-      script.defer = true;
-      document.body.appendChild(script);
+const Comments: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://cdn.commoninja.com/sdk/latest/commonninja.js";
+    script.defer = true;
+    document.body.appendChild(script);
 
-      return () => {
-        document.body.removeChild(script);
-      };
-    }, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-    return (
-      <div className={displayClass}>
-        <div className="commonninja_component pid-a50ed076-a886-4b3b-bc38-b29fa924333a"></div>
-      </div>
-    );
-  };
+  return (
+    <div className={displayClass}>
+      <div className="commonninja_component pid-a50ed076-a886-4b3b-bc38-b29fa924333a"></div>
+    </div>
+  );
+};
 
-  return Comments;
-}) satisfies QuartzComponentConstructor;
+export default (() => Comments) satisfies QuartzComponentConstructor;
