191333270e6599a43710ffd4a87d04bee345b382
Keyyard <keyyard8888@gmail.com>
2025-07-22 07:15:58 +0700

    styling + ux mobile

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,10 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <span class="title-desktop">{title}</span>
+        <span class="title-mobile">Keyyard blogs</span>
+      </a>
     </h2>
   )
 }
@@ -18,6 +21,28 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+
+.title-mobile {
+  display: none;
+}
+
+.title-desktop {
+  display: inline;
+}
+
+@media (max-width: 768px) {
+  .page-title {
+    font-size: 1.5rem;
+  }
+  
+  .title-mobile {
+    display: inline;
+  }
+  
+  .title-desktop {
+    display: none;
+  }
 }
 `
 
